a5999532db773fc0895b5885a091667c7f7fb841
Jonathan Wohl <jon@jonwohl.com>
2016-01-21 22:26:27 -0500

    boilerplate install.sh

--- install.sh
+++ install.sh
@@ -4,55 +4,29 @@
 
 echo "Hello Openframe (ExamplePlugin install.sh)"
 
+# Some limited platform detection might be in order... though at present we're targeting the Pi
 os=$(uname)
 arq=$(uname -m)
-
-# glslViewer will get cloned into 'player' dir
-mkdir player
-cd player
 
 if [ $os == "Linux" ]; then
 
     # on Debian Linux distributions
-    sudo apt-get update
-    # do we really want to upgrade? this could take a damn long time.
-    sudo apt-get upgrade
 
     # on RaspberryPi
     if [ $arq == "armv7l" ]; then
-        git clone http://github.com/patriciogonzalezvivo/glslViewer
-        cd glslViewer
-        make
-        sudo make install
-    else
-        sudo apt-get install git-core cmake xorg-dev libglu1-mesa-dev
-        git clone https://github.com/glfw/glfw.git
-        cd glfw
-        cmake .
-        make
-        sudo make install
-        cd ..
-        git clone http://github.com/patriciogonzalezvivo/glslViewer
-        cd glslViewer
-        make
-        sudo make install
-    fi
 
+        # ####
+        #
+        # FOR NOW, CODE GOES HERE since we're shooting for RPi support
+        #
+        # ####
+
+
+
+    else
+        # Non-arm7 Debian...
+    fi
 
 elif [ $os == "Darwin" ]; then
-
-    # ON MacOX
-    if [ ! -e /usr/local/bin/brew ]; then
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-    fi
-
-    brew update
-    # do we really want to upgrade? this could take a damn long time.
-    brew upgrade
-    brew tap homebrew/versions
-    brew install glfw3 pkg-config
-    git clone http://github.com/patriciogonzalezvivo/glslViewer
-    cd glslViewer
-    make
-    make install
+    # OSX
 fi
